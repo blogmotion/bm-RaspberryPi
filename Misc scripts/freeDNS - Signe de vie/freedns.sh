cfb3092e0e199b30149f9b4e00f4b1e94d6354f2
blogmotion <6735644+blogmotion@users.noreply.github.com>
2026-03-23 01:14:00 +0100

    Nouvelle version
    Correction et bonnes pratiques

--- Misc scripts/freeDNS - Signe de vie/freedns.sh
+++ Misc scripts/freeDNS - Signe de vie/freedns.sh
@@ -8,63 +8,79 @@
 # Website : https://blogmotion.fr/systeme/freedns-afraid-script-connexion-automatique-19092
 #
 # Inspire de https://gist.github.com/AnthonyWharton/a0e8faae7195a5c1dea210466eda1c92
-VERSION="2021.12.23"
+VERSION="2026.03.23"
 
-# === INFORMATIONS FREEDNS (MODIFIEZ CES VARIABLES !) :
+# === INFORMATIONS FREEDNS, MODIFIEZ CES VARIABLES :
 
 # Identifiants FreeDNS, identiques à https://freedns.afraid.org/subdomain/
-USERNAME='identifiant'
-PASSWORD='VotreMot2Passe'
+USERNAME="identifiant"     #username ou email
+PASSWORD="VotreMot2Passe"
 
 # (sous) domaine FreeDNS lisible sur https://freedns.afraid.org/subdomain/ (ID="data_id" dans l'URL)
 DOMAIN="monsous.domaine.fr"
 DOMAIN_ID="12344321"
 
-# === FIN DES VARIABLES - NE RIEN TOUCHER SOUS CETTE LIGNE ========================================================================
+# === FIN DES VARIABLES MODIFIABLES - NE RIEN TOUCHER SOUS CETTE LIGNE ========================================================================
 
+bold=$(tput bold)
 rouge=$(tput setaf 1)
 vert=$( tput setaf 2)
 cyan=$( tput setaf 6)
 reset=$(tput sgr0)
 
-shw_OK ()   { echo -e "${bold}${vert} OK! ${@}${reset}";}
-shw_err ()  { echo -e "${bold}${rouge}${@}${reset}";	}
-shw_info () { echo -e "${bold}${cyan}${@}${reset}";	}
+cleanAndExit() {
+    local code="${1:-${EXITCODE:-0}}"
+    rm -f "${COOKIE_FILE}" "${TXTID_FILE}"
+    echo && exit "${code}"
+}
 
-COOKIE_FILE="`mktemp /tmp/freedns_cookie.XXXXXXXX`"
-TXTID_FILE="`mktemp /tmp/freedns_txtid.XXXXXXXX`"
+shw_OK(){ echo -e "${bold}${vert} OK! " "$@" "${reset}"; }
+shw_info(){ echo -e "${bold}${cyan}" "$@" "${reset}"; }
+shw_err(){ echo -e "${bold}${rouge}" "$@" "${reset}"; cleanAndExit 1; }
 
+COOKIE_FILE="$(mktemp /tmp/freedns_cookie.XXXXXXXX)"
+TXTID_FILE="$(mktemp /tmp/freedns_txtid.XXXXXXXX)"
 REGEX_DOMAINID="s/.*data_id=\\([0-9]*\\)>${DOMAIN}.*/\\1/;t;d"
 
 shw_info "\n======= FreeDNS Signe de Vie v${VERSION} ========================================"
-echo -n "Connexion au site freedns..."
+echo -n "Soumission du formulaire de connexion au site freedns.afraid.org..."
 
-curl -s "https://freedns.afraid.org/zc.php?step=2" \
-     -c ${COOKIE_FILE}                              \
-     -d "action=auth"                               \
-     -d "submit=Login"                              \
-     -d "username=${USERNAME}"                      \
-     -d "password=${PASSWORD}"                      \
-     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/95.0" \
-     && shw_OK
+LOGIN_RESPONSE=$(curl -s "https://freedns.afraid.org/zc.php?step=2"	\
+     -c "${COOKIE_FILE}"                              			\
+     -d "action=auth"                               			\
+     -d "submit=Login"                              			\
+     -d "username=${USERNAME}"                      			\
+     -d "password=${PASSWORD}"                      			\
+     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0")
 
-# DEBUG:
-#curl -s "https://freedns.afraid.org/subdomain/" -b $COOKIE_FILE; exit
-
-echo -n "Recherche de l'ID du domaine..."
-
-DOM_READ_ID=$(curl -s "https://freedns.afraid.org/subdomain/" \
-                   -b $COOKIE_FILE                            \
-             | sed --posix $REGEX_DOMAINID)
-
-if [ "$DOM_READ_ID" == "$DOMAIN_ID" ]; then
-	shw_OK "(${DOM_READ_ID} = ${DOMAIN_ID})"
-	EXITCODE=0
+if echo "$LOGIN_RESPONSE" | grep -q 'error</'; then
+    shw_err "ERREUR : Échec de l'authentification (retentez plus tard si script lancé plusieurs fois)"
 else
-	shw_err " [ERREUR] ID INCORRECT:\n\t lu ${DOM_READ_ID} au lieu de l'ID attendu ${DOMAIN_ID}"
-	EXITCODE=1
+    shw_OK
 fi
 
-rm -f ${COOKIE_FILE} ${TXTID_FILE}
+# DEBUG
+#curl -s "https://freedns.afraid.org/subdomain/" -b $COOKIE_FILE > /tmp/debug.html ; exit
 
-echo && exit ${EXITCODE}
+echo -e "Controle des informations:"
+LOGGED_MEMBER_PAGE=$(curl -s "https://freedns.afraid.org/subdomain/" -b "${COOKIE_FILE}")
+
+# Lecture et concordance de l'ID membre et ID du domaine
+USER_ID=$(printf '%s' "$LOGGED_MEMBER_PAGE" | grep -Po 'UserID:</td><td bgcolor="#eeeeee" align="right">\K[^<]+(?=</td>)' || true)
+DOM_READ_ID=$(printf '%s' "$LOGGED_MEMBER_PAGE" | sed --posix "${REGEX_DOMAINID}")
+
+echo -n "-> Recherche du USERID..."
+if [ "${USER_ID}" = "$USERNAME" ]; then
+    shw_OK "(UserID=${USER_ID})"
+else
+    shw_err " ERREUR: UserID incorrect (lu=${USER_ID})"
+fi
+
+echo -n "-> Recherche de l'ID du domaine..."
+if [ "${DOM_READ_ID}" = "${DOMAIN_ID}" ]; then
+    shw_OK "(DomainID=${DOM_READ_ID})"
+else
+    shw_err " ERREUR: DomainID INCORRECT (ID lu=${DOM_READ_ID} au lieu de l'ID attendu=${DOMAIN_ID})"
+fi
+
+cleanAndExit
