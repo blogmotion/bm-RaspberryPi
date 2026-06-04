f68239a87394c6b63cc7b77f46ef9b6722c2e2a6
blogmotion <6735644+blogmotion@users.noreply.github.com>
2026-06-04 16:39:37 +0200

    Update qualiteAirAPI.sh
    correction URL API et ajout des polluants

--- Misc scripts/pollution air atmo-aura/qualiteAirAPI.sh
+++ Misc scripts/pollution air atmo-aura/qualiteAirAPI.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 # Lecture qualité air grenoble via API Atmo (http://api.atmo-aura.fr/documentation)
 # Date: 2018.09.03
-# Auteur: @xhark - http://blogmotion.fr/diy/pollution-grenoble-api-atmo-aura-17474
+# Udp : 2026.06.04
+# Auteur: @xhark - https://blogmotion.fr/diy/pollution-grenoble-api-atmo-aura-17474
 # Licence CC
 
 APIKEY="xxxxxxxxxxxxxxxxxxxxxx"	# Votre cle API Atmo-Aura
 INSEEC="38185"					# Code INSEE commune 38185=Grenoble - https://bit.ly/codeinsee
 
 ### NE RIEN TOUCHER SOUS CETTE LIGNE #####################################################################
-which jq 2>&1 >/dev/null || (echo "ERREUR: jq absent, essayez: sudo apt install jq"; exit 1)
+URL="https://api.atmo-aura.fr/api/v1/communes/${INSEEC}/indices/atmo?api_token=${APIKEY}&date_echeance=now"
+AUJ=$(date +%Y-%m-%d)
 
-URLINDICE="https://api.atmo-aura.fr/communes/${INSEEC}/indices?date=now&api_token=${APIKEY}"
-JSON=$(curl -L -s --connect-timeout 5 $URLINDICE) || CURL_RETURN_CODE=$?
+JSON=$(curl -L -s --connect-timeout 5 $URL) || CURL_RETURN_CODE=$?
 
 if [[ ${CURL_RETURN_CODE} -ne 0 ]]; then
-	echo "[qualiteAirAPI] Erreur de connexion, verifiez l'URL. Code de retour cURL=${CURL_RETURN_CODE} (https://bit.ly/curlerr)"; exit 1
-else
-	if [[ ${JSON} = *"bad_token"* ]]; then
-        	echo "[qualiteAirAPI] Erreur de token, verifiez la variable APIKEY"; exit 1
-	fi
+        echo "[qualiteAirAPI] Erreur de connexion, verifiez l'URL. Code de retour cURL=${CURL_RETURN_CODE} (https://bit.ly/curlerr)";
+        exit 1
 fi
 
-# tonumber car string en entree et erreur "number required" sinon, floor pour arrondir 34.4393939 => 34
-POURCENTAIR=$(echo "$JSON" | jq -r '.indices .valeur | tonumber | floor')
-QUALITE2AIR=$(echo "$JSON" | jq -r '.indices .qualificatif | ascii_downcase')
+if [[ "${JSON}" =~ (bad_token|INSUFFICIENT_ROLE) ]]; then
+        echo -e "[qualiteAirAPI] Erreur de token/role, verifiez la variable APIKEY. Retour :\n\n ${JSON}"; exit 1
+fi
 
-# si appel du script avec 1 argument alors on retourne juste le % (cas injection dans domoticz)
+QUALITE2AIR=$(echo "$JSON"      | jq --arg jqAuj "$AUJ" -r '.data | map(select(.date_echeance == $jqAuj)) | .[] | .qualificatif')
+INDICEAIR=$(echo "$JSON"        | jq -r '.data[] .indice')
+
+# On selectionne le polluant majoritaire, puis ses caracteristiques : indice, concentration
+POLMAJO_NOM=$(echo "$JSON"      | jq -r '.data[] .polluants_majoritaires[0]')
+POLMAJO_INDICE=$(echo "$JSON"   | jq --arg jqPollMaj "$POLMAJO_NOM" -r '.data[] | .sous_indices | map(select(.polluant_nom == $jqPollMaj)) | .[] .indice')
+POLMAJO_CONCTR=$(echo "$JSON"   | jq --arg jqPollMaj "$POLMAJO_NOM" -r '.data[] | .sous_indices | map(select(.polluant_nom == $jqPollMaj)) | .[] .concentration | tonumber | floor')
+
+# si appel du script avec 1 argument (comme "brut") alors on retourne juste le %
 if [[ $# -eq "1" ]]; then
-	echo ${POURCENTAIR} ; exit 0
+        echo ${POLMAJO_CONCTR}
+        exit 0
 fi
 
-# En cas de pollution importante >  90 on met en MAJUSCULES
-if [[ "${POURCENTAIR}" -ge "91" ]]; then
-	QUALITE2AIR=$(echo $QUALITE2AIR | awk '{print(toupper($0))}')
+# En cas de pollution importante >=  4 on met en MAJUSCULES -- Echelle de 1 à 6 (mauvais) https://api.atmo-aura.fr/api/v1/indices/atmo/definitions?api_token=${APIKEY}
+if [[ "${INDICE}" -ge "4" ]]; then
+        QUALITE2AIR=$(echo $QUALITE2AIR | awk '{print(toupper($0))}')
 fi
 
-echo "Air ${QUALITE2AIR}, pollution ${POURCENTAIR}% (via API)." ; exit 0
+echo  "Air ${QUALITE2AIR} (${INDICEAIR}/6). Major: ${POLMAJO_NOM} (${POLMAJO_INDICE}/6)"
+
+exit 0
